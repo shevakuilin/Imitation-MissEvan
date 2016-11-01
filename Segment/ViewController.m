025db2a669693706c0fb53264e110eb2894a7ed2
shevakuilin <shevakuilin@gmail.com>
2016-11-01 18:12:20 +0800

    add segmentOther

---
+++ Segment/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  XHSegmentControllerSample
+//
+//  Created by xihe on 16/4/19.
+//  Copyright © 2016年 xihe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end

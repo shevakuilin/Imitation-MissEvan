872b013ce978db3619c34aa007e3595c8c7c0801
shevakuilin <shevakuilin@gmail.com>
2016-12-16 12:07:43 +0800

    睡觉猫状态管理完成

--- Imitation-MissEvan/Imitation-MissEvan/Structure/METabBarCatImageView.m
+++ Imitation-MissEvan/Imitation-MissEvan/Structure/METabBarCatImageView.m
@@ -53,8 +53,50 @@
     BOOL isPlay = [[sender userInfo][@"isPlay"] boolValue];
     if (isPlay == YES) {
         MELog(@"正在播放");
+        NSMutableArray * stackImageArray = [NSMutableArray new];
+        for (NSInteger i = 1; i < 72; i ++) {
+            NSString * stackImageName;
+            if (i < 10) {
+                stackImageName = [NSString stringWithFormat:@"miao2000%@_200x200_@1x", @(i)];
+            } else {
+                stackImageName = [NSString stringWithFormat:@"miao200%@_200x200_@1x", @(i)];
+            }
+            UIImage * imageName = [UIImage imageNamed:stackImageName];
+            [stackImageArray addObject:imageName];
+            
+            self.animationImages = stackImageArray;
+            //动画重复次数
+            self.animationRepeatCount = 10000000 * 10000000;
+            //动画执行时间,多长时间执行完动画
+            self.animationDuration = 8.0;
+            //开始动画
+            [self startAnimating];
+        }
+
     } else {
         MELog(@"播放暂停");
+        NSMutableArray * stackImageArray = [NSMutableArray new];
+        for (NSInteger i = 1; i < 141; i ++) {
+            NSString * stackImageName;
+            if (i < 10) {
+                stackImageName = [NSString stringWithFormat:@"DRRR猫 睡觉000%@_200x200_@1x", @(i)];
+            } else if (i > 9 && i < 100){
+                stackImageName = [NSString stringWithFormat:@"DRRR猫 睡觉00%@_200x200_@1x", @(i)];
+            } else {
+                stackImageName = [NSString stringWithFormat:@"DRRR猫 睡觉0%@_200x200_@1x", @(i)];
+            }
+            UIImage * imageName = [UIImage imageNamed:stackImageName];
+            [stackImageArray addObject:imageName];
+            
+            self.animationImages = stackImageArray;
+            //动画重复次数
+            self.animationRepeatCount = 10000000 * 10000000;
+            //动画执行时间,多长时间执行完动画
+            self.animationDuration = 8.0;
+            //开始动画
+            [self startAnimating];
+        }
+
     }
 }
 
